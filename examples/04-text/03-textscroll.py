5b8b70102ce510bcc1a9e41d5f8de795525b0229
Christopher Arndt <chris@chrisarndt.de>
2015-10-15 11:11:16 +0200

    use simple drop-shadow text effect; put calculations in 'setup' and 'update' methods; handle window resize; use screen aspect ratio for initial window size

--- examples/04-text/03-textscroll.py
+++ examples/04-text/03-textscroll.py
@@ -13,41 +13,42 @@ class MyCanvas(Canvas):
     def __init__(self, *args, **kwargs):
         Canvas.__init__(self, *args, **kwargs)
         txt = Text("NodeBox for OpenGL",
+                   x=2, y=30,
                    font="Times New Roman",
                    fontsize=75,
                    fontweight=BOLD,
                    lineheight=1.2,
-                   fill=color(0.2, 0.2, 255 / 128))
+                   fill=color(0.))
 
         def render_text():
-            text(txt, 0, 30)
+            text(txt)
 
-        img = render(render_text, textwidth(txt) + 20, 110)
-        self.img = bloom(img, intensity=3.0)
-        shadow = desaturate(img)
-        self.shadow = blur(shadow, amount=3.0, kernel=5)
-        self.speed = 1.5  # pixels/second
-        self.pos = -self.img.height
+        self.img = render(render_text, textwidth(txt) + 20, 110)
+        self.shadow = dropshadow(self.img, amount=3.0)
+        self.speed = 50  # pixels/second
 
-    def on_key_press(self, keys):
-        Canvas.on_key_press(self, keys)
+    def on_resize(self):
+        self.text_x = (self.width - self.img.width) / 2
 
-        if keys.code == 'f11':
-            self.fullscreen = not self.fullscreen
+    def setup(self):
+        self.text_x = (self.width - self.img.width) / 2
+        self.text_y = -self.img.height
+        self.mouse.cursor = HIDDEN
+
+    def update(self):
+        self.text_y += self.speed * self.elapsed
+
+        if self.text_y > self.height:
+            self.text_y = -self.img.height
 
     def draw(self):
-        x = (self.width - self.img.width) / 2
-        y = self.pos
-
         self.clear()
-        background(0.0)
-        image(self.shadow, x=x + 5, y=y - 5, alpha=0.7)
-        image(self.img, x, y)
-
-        self.pos += self.speed
-        if self.pos > self.height:
-            self.pos = -self.img.height
+        background(1.0)
+        image(self.shadow, self.text_x + 4, self.text_y - 4)
+        image(self.img, self.text_x, self.text_y)
 
 
-canvas = MyCanvas(width=1024, height=600)
+canvas = MyCanvas()
+aspect = canvas.screen.width / canvas.screen.height
+canvas.size = (1024, int(1024 * (1.0 / aspect)))
 canvas.run()
